cc427ce6a3c8abffefafed59af1aa6d2a0c3d717
Jamie <jperrelet@gmail.com>
2022-10-18 13:35:34 -0700

    Added Has_components trait

---
+++ has-components.trait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Digitalis;
+
+trait Has_Components {
+
+    protected $components = [];
+
+    public function component ($class_name, $params = [], $render = true) {
+
+        $class_name = 'Digitalis\\' . $class_name;
+
+        if (!class_exists($class_name)) return;
+
+        $component = new $class_name($params);
+        $component->init();
+        
+        if ($render) $component->render();
+
+        $key = $class_name . "-" . $component->get_instance();
+        $this->components[$key] = $component;
+
+        return $component;
+
+    }
+
+    public function get_component ($class_name, $instance = 1) {
+
+        $class_name = 'Digitalis\\' . $class_name;
+        $key = $class_name . "-" . $instance;
+
+        return isset($this->components[$key]) ? $this->components[$key] : null;
+
+    }
+
+}
