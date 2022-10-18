75bdb29b52f87193a1b95ffcc61d5aec87a09207
Jamie <jperrelet@gmail.com>
2022-10-18 13:37:32 -0700

    Added Has_Components trait to loaded

--- load.php
+++ load.php
@@ -5,6 +5,8 @@ if (defined('DIGITALIS_FRAMEWORK_VERSION')) return;
 define('DIGITALIS_FRAMEWORK_VERSION', 	'0.0.0');
 define('DIGITALIS_FRAMEWORK_PATH', 	plugin_dir_path( __FILE__ ) );
 
+require DIGITALIS_FRAMEWORK_PATH . 'has-components.trait.php';
+
 require DIGITALIS_FRAMEWORK_PATH . 'base.abstract.php';
 
 require DIGITALIS_FRAMEWORK_PATH . 'post-type.abstract.php';
