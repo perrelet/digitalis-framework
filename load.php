b6c9fa1620c3ee4ffe1ce14b0c12c2da42065161
Jamie <jperrelet@gmail.com>
2022-10-18 15:01:20 -0700

    Has_Integrations trait added to loader

--- load.php
+++ load.php
@@ -6,6 +6,7 @@ define('DIGITALIS_FRAMEWORK_VERSION', 	'0.0.0');
 define('DIGITALIS_FRAMEWORK_PATH', 	plugin_dir_path( __FILE__ ) );
 
 require DIGITALIS_FRAMEWORK_PATH . 'has-components.trait.php';
+require DIGITALIS_FRAMEWORK_PATH . 'has-integrations.trait.php';
 
 require DIGITALIS_FRAMEWORK_PATH . 'base.abstract.php';
 
