3c7a4992e29390244268011a172adb64a601055e
Jamie <jperrelet@gmail.com>
2022-10-18 13:48:05 -0700

    Singleton class

---
+++ singleton.abstract.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Digitalis;
+
+abstract class Singleton {
+
+    private static $instances = [];
+
+    protected function __construct() { }
+    protected function __clone() { }
+
+    public function __wakeup(){ throw new \Exception("You may not dream me into existence."); }
+
+    public static function getInstance(): Singleton {
+
+        $class = static::class;
+
+        if (!isset(self::$instances[$class])) {
+            self::$instances[$class] = new static();
+            self::$instances[$class]->run();
+        }
+
+        return self::$instances[$class];
+        
+    }
+
+}
