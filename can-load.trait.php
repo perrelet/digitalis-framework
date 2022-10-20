b7f620efe7e667cf6ce90525050165b5c6772051
Jamie <jperrelet@gmail.com>
2022-10-20 15:29:00 -0400

    Switched autoloader to serializer

--- can-load.trait.php
+++ can-load.trait.php
@@ -4,46 +4,34 @@ namespace Digitalis;
 
 trait Can_Load {
 
-    protected $loaded_classes = [];
     protected $object_groups = [];
 
-    public function autoload ($group_key, $dir_path, $ext = 'php') {
+    public function autoload ($group_key, $dir_path, $ext = 'php', $instantiate = true) {
 
         foreach (glob($dir_path . '/*.' . $ext) as $path) {
 
-            $this->load_class($group_key, $path);
+            $this->load_class($group_key, $path, $instantiate);
 
         }
 
     }
 
-    public function load_class ($group_key, $file_path) {
+    public function load_class ($group_key, $file_path, $instantiate = true) {
 
         if (!is_file($file_path)) return false;
 
         include $file_path;
-        $this->loaded_classes[] = $group_key;
 
-        return true;
-
-    }
-
-    public function instantiate () {
-
-        if (!$this->loaded_classes) return false;
-
-        $classes = get_declared_classes();
-
-        foreach ($this->loaded_classes as $i => $group_key) {
+        if ($instantiate !== false) {
+         
+            if (!$class_name = $this->get_class_name($file_path)) return false;
 
             if (!isset($this->object_groups[$group_key])) $this->object_groups[$group_key] = [];
-
-            $class = $classes[count($classes) - $i - 1];
-            $this->object_groups[$group_key][$class] = new $class();
+            $this->object_groups[$group_key][$class_name] = new $class_name();
 
         }
 
-        $this->loaded_classes = [];
+        return true;
 
     }
 
@@ -65,6 +53,46 @@ trait Can_Load {
 
         return $this->object_groups[$group_key][$class_name];
 
-    } 
+    }
+
+    //
+
+    protected function get_class_name ($file) {
+
+        $fp = fopen($file, 'r');
+        $class = $namespace = $buffer = '';
+        $i = 0;
+        while (!$class) {
+            if (feof($fp)) break;
+
+            $buffer .= fread($fp, 512);
+            $tokens = token_get_all($buffer);
+
+            if (strpos($buffer, '{') === false) continue;
+
+            for (;$i<count($tokens);$i++) {
+                if ($tokens[$i][0] === T_NAMESPACE) {
+                    for ($j=$i+1;$j<count($tokens); $j++) {
+                        if ($tokens[$j][0] === T_STRING) {
+                            $namespace .= '\\'.$tokens[$j][1];
+                        } else if ($tokens[$j] === '{' || $tokens[$j] === ';') {
+                            break;
+                        }
+                    }
+                }
+
+                if ($tokens[$i][0] === T_CLASS) {
+                    for ($j=$i+1;$j<count($tokens);$j++) {
+                        if ($tokens[$j] === '{') {
+                            $class = $tokens[$i+2][1];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $namespace ? $namespace . "\\" . $class : $class;
+
+    }
 
 }
