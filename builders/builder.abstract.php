7843bfb06f386d1672ef830e322a31e3502381cd
Jamie <jperrelet@gmail.com>
2023-01-18 19:53:27 +0000

    Added section position options

--- builders/builder.abstract.php
+++ builders/builder.abstract.php
@@ -30,7 +30,7 @@ abstract class Builder implements Builder_Interface {
         'hide-lt-page', 'hide-lt-tablet', 'hide-lt-phone-landscape', 'hide-lt-phone-portrait', 
         'hide-gt-page', 'hide-gt-tablet', 'hide-gt-phone-landscape', 'hide-gt-phone-portrait',
 
-        'center-section', 'relative-section', 'merge', 'screen-height', 'explode',
+        'top-section', 'center-section', 'bottom-section', 'stretch-section', 'relative-section', 'merge', 'screen-height', 'explode',
         'cols-2', 'cols-stretch', 'cols-2-page', 'phone-landscape', 'swap', 'remove-first', 'remove-last',
 
         'cover-pseudo', 'cover-before', 'cover-after', 'cover-both',
