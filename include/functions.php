f1622bf1625da51648220413ea5a12179718dbdc
Jamie <jperrelet@gmail.com>
2023-03-07 16:59:37 -0800

    field_not_empty global

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// ACF: Gets a field from a WP_Term
+
 if (!function_exists('get_tax_field')) {
 
     function get_tax_field ($selector, $term_id = false) {
@@ -10,6 +12,8 @@ if (!function_exists('get_tax_field')) {
 
 }
 
+// ACF: Plucks a signle element from a field that returns an Array
+
 if (!function_exists('get_field_element')) {
 
     function get_field_element ($selector, $key, $post_id = false, $format_value = true) {
@@ -19,3 +23,17 @@ if (!function_exists('get_field_element')) {
     }
 
 }
+
+// ACF: Checks if a field has a value.
+// This is helpful because wp-includes/shortcodes.php throws an Array to string conversion warning for fields that return arrays when using conditions.
+
+if (!function_exists('field_not_empty')) {
+
+    function field_not_empty ($selector, $post_id = false, $format_value = true) {
+
+        return !empty(get_field($selector, $post_id, $format_value));
+
+    }
+
+}
+
