96555b3374f8b30dadfe9d8488973adb437a8574
jamie <jperrelet@gmail.com>
2023-04-08 17:38:10 +0100

    Indent: tab -> space

--- include/wordpress/taxonomy.abstract.php
+++ include/wordpress/taxonomy.abstract.php
@@ -14,7 +14,7 @@ abstract class Taxonomy extends Base {
     public function __construct ($flush = false) {
 
         if ($flush) flush_rewrite_rules();
-        
+
         add_action('init', [$this, 'register']);
 
     }
@@ -29,11 +29,11 @@ abstract class Taxonomy extends Base {
         $args = apply_filters("digitalis-" . $this->get_identifier() . "-args", $args);
 
         register_taxonomy(
-			$this->slug,
-			$this->post_types,
-			$args
-		);
-        
+            $this->slug,
+            $this->post_types,
+            $args
+        );
+
     }
 
     protected function get_args ($args) {           // You might override this...
@@ -57,23 +57,23 @@ abstract class Taxonomy extends Base {
     protected function get_default_args () {
 
         return [
-			'hierarchical'				=> true,
-			'public'					=> true,
-			'show_ui'					=> true,
-			'show_in_menu'				=> true,
-			'show_admin_column'			=> true,
-			'show_in_nav_menus'			=> true,
-			'show_tagcloud'				=> false,
-			'show_in_rest' 				=> true,
-		];
+            'hierarchical'              => true,
+            'public'                    => true,
+            'show_ui'                   => true,
+            'show_in_menu'              => true,
+            'show_admin_column'         => true,
+            'show_in_nav_menus'         => true,
+            'show_tagcloud'             => false,
+            'show_in_rest'              => true,
+        ];
 
     }
 
     protected function get_default_rewrite () {
 
         return [
-            'slug'			=> $this->slug,
-            'with_front'	=> false
+            'slug'          => $this->slug,
+            'with_front'    => false
         ];
 
     }
@@ -82,26 +82,26 @@ abstract class Taxonomy extends Base {
 
         return apply_filters("digitalis-" . $this->get_identifier() . "-labels",
         [
-            'name'							=> __( $this->singular, 							$this->text_domain ),
-			'singular_name'					=> __( $this->singular, 							$this->text_domain ),
-			'menu_name'						=> __( $this->plural, 						    $this->text_domain ),
-			'all_items'						=> __( "All {$this->plural}", 					$this->text_domain ),
-			'parent_item'					=> __( "Parent {$this->singular}", 				$this->text_domain ),
-			'parent_item_colon'				=> __( "Parent {$this->singular}:", 				$this->text_domain ),
-			'new_item_name'					=> __( "New {$this->singular}", 					$this->text_domain ),
-			'add_new_item'					=> __( "Add New {$this->singular}", 				$this->text_domain ),
-			'edit_item'						=> __( "Edit {$this->singular}", 					$this->text_domain ),
-			'update_item'					=> __( "Update {$this->singular}", 				$this->text_domain ),
-			'view_item'						=> __( "View {$this->singular}", 					$this->text_domain ),
-			'separate_items_with_commas'	=> __( "Separate {$this->plural} with commas",	$this->text_domain ),
-			'add_or_remove_items'			=> __( "Add or remove {$this->plural}", 		$this->text_domain ),
-			'choose_from_most_used'			=> __( "Choose from the most used", 	        $this->text_domain ),
-			'popular_items'					=> __( "Popular {$this->plural}", 				$this->text_domain ),
-			'search_items'					=> __( "Search {$this->plural}", 				$this->text_domain ),
-			'not_found'						=> __( "Not Found", 					        $this->text_domain ),
-			'no_terms'						=> __( "No {$this->plural}", 					$this->text_domain ),
-			'items_list'					=> __( "List of {$this->plural}", 				$this->text_domain ),
-			'items_list_navigation'			=> __( "{$this->plural} list navigation", 		$this->text_domain ),
+            'name'                          => __( $this->singular,                         $this->text_domain ),
+            'singular_name'                 => __( $this->singular,                         $this->text_domain ),
+            'menu_name'                     => __( $this->plural,                           $this->text_domain ),
+            'all_items'                     => __( "All {$this->plural}",                   $this->text_domain ),
+            'parent_item'                   => __( "Parent {$this->singular}",              $this->text_domain ),
+            'parent_item_colon'             => __( "Parent {$this->singular}:",             $this->text_domain ),
+            'new_item_name'                 => __( "New {$this->singular}",                 $this->text_domain ),
+            'add_new_item'                  => __( "Add New {$this->singular}",             $this->text_domain ),
+            'edit_item'                     => __( "Edit {$this->singular}",                $this->text_domain ),
+            'update_item'                   => __( "Update {$this->singular}",              $this->text_domain ),
+            'view_item'                     => __( "View {$this->singular}", 	            $this->text_domain ),
+            'separate_items_with_commas'    => __( "Separate {$this->plural} with commas",  $this->text_domain ),
+            'add_or_remove_items'           => __( "Add or remove {$this->plural}",         $this->text_domain ),
+            'choose_from_most_used'         => __( "Choose from the most used",             $this->text_domain ),
+            'popular_items'                 => __( "Popular {$this->plural}",               $this->text_domain ),
+            'search_items'                  => __( "Search {$this->plural}",                $this->text_domain ),
+            'not_found'                     => __( "Not Found",                             $this->text_domain ),
+            'no_terms'                      => __( "No {$this->plural}",                    $this->text_domain ),
+            'items_list'                    => __( "List of {$this->plural}",               $this->text_domain ),
+            'items_list_navigation'         => __( "{$this->plural} list navigation",       $this->text_domain ),
         ]);
 
     }
