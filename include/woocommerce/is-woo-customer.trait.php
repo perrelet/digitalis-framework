82212d09d475f16b3e3b0e03b8b2c419b100f623
jamie <jperrelet@gmail.com>
2023-05-01 23:41:02 +0100

    Is_Woo_Customer::get_orders 🛒

--- include/woocommerce/is-woo-customer.trait.php
+++ include/woocommerce/is-woo-customer.trait.php
@@ -22,4 +22,20 @@ trait Is_Woo_Customer {
 
     }
 
+    public function get_orders ($args = []) {
+
+        $args = wp_parse_args($args, [
+            'status'        => 'paid',
+        ]);
+
+        $args['customer_id'] = $this->id;
+
+        if ($args['status'] == 'paid')              $args['status'] = wc_get_is_paid_statuses();
+        if ($args['status'] == 'pending')           $args['status'] = wc_get_is_pending_statuses();
+        if ($args['status'] == 'paid_and_pending')  $args['status'] = array_merge(wc_get_is_paid_statuses(), wc_get_is_pending_statuses());
+
+        return wc_get_orders($args);
+
+    }
+
 }
