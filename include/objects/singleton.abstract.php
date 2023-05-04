fe129b3e17ae0bfd9201a4c867739017fae43579
jamie <jperrelet@gmail.com>
2023-05-04 20:53:38 +0100

    Depreciated run method

--- include/objects/singleton.abstract.php
+++ include/objects/singleton.abstract.php
@@ -11,21 +11,27 @@ abstract class Singleton {
 
     public function __wakeup(){ throw new \Exception("You may not dream me into existence."); }
 
+    public static function get_class_name () {
+
+        return apply_filters('Digitalis/Class/' . static::class, static::class);
+
+    }
+
     public static function get_instance () {
 
-        $class = static::class;
+        $class_name = static::get_class_name();
 
-        if (!isset(self::$instances[$class])) {
-            self::$instances[$class] = new static();
-            self::$instances[$class]->run();
-            self::$instances[$class]->init();
+        if (!isset(self::$instances[$class_name])) {
+            self::$instances[$class_name] = new static();
+            //self::$instances[$class_name]->run();
+            self::$instances[$class_name]->init();
         }
 
-        return self::$instances[$class];
+        return self::$instances[$class_name];
         
     }
 
-    public function run () {} // Depreciated
+    //public function run () {} // Depreciated
     public function init () {} // Override me :)
 
 }
