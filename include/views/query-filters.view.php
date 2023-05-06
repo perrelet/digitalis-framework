3b2915a81bc0e6ab328b8ad69ebf1e7c743d4e9f
jamie <jperrelet@gmail.com>
2023-05-06 02:54:31 +0100

    A query field group 🔎

---
+++ include/views/query-filters.view.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Digitalis;
+
+abstract class Query_Filters extends Field_Group {
+
+    protected static $defaults = [
+        'archive_id'        => 'digitalis-archive',
+        'module_version'    => DIGITALIS_FRAMEWORK_VERSION,
+        'module_url'        => DIGITALIS_FRAMEWORK_URI . 'modules/query.js',
+        'action'            => 'query_[post_type]',
+        'fields'            => [],
+        'tag'               => 'form',
+        'attributes'        => [],
+        'js_params'         => [],
+    ];
+
+    protected static function before_first ($p) {
+
+        $js_params = wp_parse_args($p['js_params'], [
+            'ajax_url'          => admin_url('admin-ajax.php'),
+            'action'            => $p['action'],
+            'archive_id'        => $p['archive_id'],
+        ]);
+
+        $handle = static::class . '-query-module';
+
+        wp_enqueue_script($handle, $p['module_url'], [], $p['module_version'], true);
+        wp_localize_script($handle, 'query_params', $js_params);
+
+    }
+
+}
