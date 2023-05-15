6c4b8180553ba45188641e0c9398ba69ec157684
jamie <jperrelet@gmail.com>
2023-05-15 23:18:12 +0100

    Is_Woo_Customer::has_ordered 🛒

--- include/woocommerce/is-woo-customer.trait.php
+++ include/woocommerce/is-woo-customer.trait.php
@@ -3,6 +3,7 @@
 namespace Digitalis;
 
 use \WC_Customer;
+use \WC_Order_Item_Product;
 
 trait Is_Woo_Customer {
 
@@ -35,6 +36,22 @@ trait Is_Woo_Customer {
         if ($args['status'] == 'paid_and_pending')  $args['status'] = array_merge(wc_get_is_paid_statuses(), wc_get_is_pending_statuses());
 
         return wc_get_orders($args);
+
+    }
+
+    public function has_ordered ($product_id) {
+
+        if ($orders = $this->get_orders()) foreach ($orders as $order) {
+            
+            if ($items = $order->get_items()) foreach ($items as $item) {
+
+                if (($item instanceof WC_Order_Item_Product) && ($product_id == $item->get_product_id())) return $order;
+
+            }
+
+        }
+
+        return false;
 
     }
 
