a1a52e9744afc1d53b4a611ff6ac949c323d0d90
Jamie <jperrelet@gmail.com>
2023-07-05 14:57:49 +0000

    select_all option ☑️✅✅

--- include/views/fields/checkbox-group.field.php
+++ include/views/fields/checkbox-group.field.php
@@ -9,7 +9,24 @@ class Checkbox_Group extends Field {
     protected static $template = 'checkbox-group';
 
     protected static $defaults = [
-        'type' => 'checkbox-group',
+        'type'          => 'checkbox-group',
+        'select_all'    => false,
     ];
+
+    public static function params ($p) {
+        
+        $p = parent::params($p);
+
+        if ($p['select_all']) {
+
+            $p['options'] = array_merge([
+                'all' => is_string($p['select_all']) ? $p['select_all'] : 'Select All',
+            ], $p['options']);
+
+        }
+
+        return $p;
+        
+    }
 
 }
