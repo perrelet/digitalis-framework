c9f712ff2e8f94733999a44b43a116dd7d8c55a2
Jamie <jperrelet@gmail.com>
2023-08-09 08:43:39 +0000

    <optgroup> support ⚙️👪

--- templates/digitalis/fields/select.php
+++ templates/digitalis/fields/select.php
@@ -1,5 +1,13 @@
 <select id='<?= $id ?>' name='<?= $key ?>' <?= $attributes ?>>
-<?php if ($options) foreach ($options as $option => $option_label): ?>
-    <option value='<?= $option ?>' <?= selected($value, $option, false) ?>><?= $option_label ?></option>
+<?php if ($options) foreach ($options as $option_key => $option): ?>
+    <?php if (is_array($option)): ?>
+        <optgroup label='<?= $option_key ?>'>
+        <?php if ($option) foreach ($option as $sub_option_key => $sub_option): ?>
+            <option value='<?= $sub_option_key ?>' <?= selected($value, $sub_option_key, false) ?>><?= $sub_option ?></option>
+        <?php endforeach; ?>
+        </optgroup>
+    <?php else: ?>
+    <option value='<?= $option_key ?>' <?= selected($value, $option_key, false) ?>><?= $option ?></option>
+    <?php endif; ?>
 <?php endforeach; ?>
 </select>
