1c7c913c25783b6f7dd9ac2f6cc6f78dcf5f106b
Jamie <jperrelet@gmail.com>
2023-08-10 09:49:33 +0000

    Prefix endpoint html/ + Respond plain txt 🗣️✈️🖊️

--- include/objects/view-route.abstract.php
+++ include/objects/view-route.abstract.php
@@ -9,6 +9,10 @@ abstract class View_Route extends Route {
 
     protected $view = View::class;
 
+    protected $namespace_prefix = 'html/';
+
+    protected static $instances = 0;
+
     /* protected function get_params () {
         
         return [
@@ -22,6 +26,22 @@ abstract class View_Route extends Route {
         
     } */
 
+    public function __construct () {
+        
+        parent::__construct();
+
+        if (++static::$instances == 1) add_filter('rest_pre_serve_request', [$this, 'rest_pre_serve_request'], 10, 4);
+        
+    }
+
+    public function register_api_routes () {
+
+        parent::register_api_routes();
+
+        register_rest_route($this->namespace_prefix . $this->namespace, $this->route, $this->rest_args);
+        
+    }
+
     public function callback (WP_REST_Request $request) {
 
         if (!is_subclass_of($this->view, View::class)) return $this->respond(new WP_Error('view-error', "\$view must be a subclass of \Digitalis\View, '{$this->view}' provided."));
@@ -31,6 +51,23 @@ abstract class View_Route extends Route {
 
         return $this->respond(call_user_func("{$this->view}::render", $params, false));
 
+    }
+
+    public function rest_pre_serve_request ($served, $result, $request, $wp_rest_server) {
+
+        if (substr($request->get_route(), 0, strlen($this->namespace_prefix) + 1) == '/' . $this->namespace_prefix) {
+            
+            $embed = isset($_GET['_embed']) ? rest_parse_embed_param($_GET['_embed']) : false;
+            $result = $wp_rest_server->response_to_data($result, $embed);
+    
+            print_r($result);
+
+            return true;
+    
+        }
+    
+        return $served;
+        
     }
 
 }
