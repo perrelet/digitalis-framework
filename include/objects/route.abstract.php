ea41bc017da12c31b0f0ab49f9f507e48f4c7e4d
Jamie <jperrelet@gmail.com>
2023-08-12 12:39:37 +0000

    set_wp_query_vars 👇❓📦

--- include/objects/route.abstract.php
+++ include/objects/route.abstract.php
@@ -9,6 +9,7 @@ abstract class Route {
 
     protected $namespace = 'digitalis/v1';
     protected $route     = 'route';
+    protected $wp_query  = false;
 
     protected $rest_args = [];
 
@@ -16,6 +17,21 @@ abstract class Route {
 
         add_action('rest_api_init', [$this, 'register_api_routes']);
 
+        if ($this->wp_query) add_filter('rest_request_before_callbacks', [$this, 'set_wp_query_vars'], 10, 3);
+
+    }
+
+    public function set_wp_query_vars ($response, $handler, $request) {
+
+        if ($this->is_this_route($request)) {
+
+            global $wp, $wp_query;
+            $wp_query->query_vars = $wp->query_vars;
+
+        }
+
+        return $response;
+        
     }
 
     protected function get_params () {
@@ -43,7 +59,7 @@ abstract class Route {
     }
 
     public function register_api_routes () {
-        
+
         $this->rest_args = $this->get_rest_args();
 
         register_rest_route($this->namespace, $this->route, $this->rest_args);
@@ -66,6 +82,14 @@ abstract class Route {
         
         return rest_ensure_response($response);
         
+    }
+
+    //
+
+    protected function is_this_route (WP_REST_Request $request) {
+
+        return (ltrim($request->get_route(), "/") == "{$this->namespace}/{$this->route}");
+
     }
 
 }
