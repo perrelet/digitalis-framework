7e4e3e946bec1a0a03d329ce12e86f98d1369be4
Jamie <jperrelet@gmail.com>
2023-10-31 20:43:37 +0000

    refactor: move htmx logic to parent class 🚚🧠👪

--- include/objects/route.abstract.php
+++ include/objects/route.abstract.php
@@ -5,19 +5,24 @@ namespace Digitalis;
 use WP_REST_Request;
 use WP_Error;
 
-abstract class Route {
+abstract class Route extends Singleton {
 
-    protected $namespace = 'digitalis/v1';
-    protected $route     = 'route';
-    protected $wp_query  = false;
+    protected $namespace        = 'digitalis/v1';
+    protected $route            = 'route';
+    protected $wp_query         = false;
+    protected $namespace_prefix = 'html/';
 
     protected $rest_args = [];
+
+    protected static $instances = 0;
 
     public function __construct () {
 
         add_action('rest_api_init', [$this, 'register_api_routes']);
 
         if ($this->wp_query) add_filter('rest_request_before_callbacks', [$this, 'set_wp_query_vars'], 10, 3);
+
+        if (++static::$instances == 1) add_filter('rest_pre_serve_request', [$this, 'rest_pre_serve_request'], 10, 4);
 
     }
 
@@ -63,6 +68,7 @@ abstract class Route {
         $this->rest_args = $this->get_rest_args();
 
         register_rest_route($this->namespace, $this->route, $this->rest_args);
+        if ($this->namespace_prefix) register_rest_route($this->namespace_prefix . $this->namespace, $this->route, $this->rest_args);
         
     }
 
@@ -84,11 +90,65 @@ abstract class Route {
         
     }
 
+    public function rest_pre_serve_request ($served, $result, $request, $wp_rest_server) {
+
+        if (substr($request->get_route(), 0, strlen($this->namespace_prefix) + 1) == '/' . $this->namespace_prefix) {
+            
+            $embed = isset($_GET['_embed']) ? rest_parse_embed_param($_GET['_embed']) : false;
+            $result = $wp_rest_server->response_to_data($result, $embed);
+    
+            print_r($result);
+
+            return true;
+    
+        }
+    
+        return $served;
+        
+    }
+
+    //
+
+    public static function get_url ($query_params = [], $html = true, $nonce = true) {
+
+        $self = static::inst();
+
+        $url = $html ? "{$self->namespace_prefix}{$self->namespace}/{$self->route}" : "{$self->namespace}/{$self->route}";
+        $url = get_rest_url(null, $url);
+        if ($query_params) $url = add_query_arg($query_params, $url);
+        if ($nonce) $url = static::nonce_url($url);
+    
+        return $url;
+    
+    }
+
+    public static function nonce_url ($url) {
+    
+        $url = add_query_arg('_wpnonce', static::get_nonce(), $url);
+        $url = str_replace("%25post_id%25", "%post_id%", $url);
+
+        return $url;
+    
+    }
+
+    public static $nonce;
+
+    public static function get_nonce () {
+        
+        if (!static::$nonce) static::$nonce = wp_create_nonce('wp_rest');
+
+        return static::$nonce;
+        
+    }
+
     //
 
     protected function is_this_route (WP_REST_Request $request) {
 
-        return (ltrim($request->get_route(), "/") == "{$this->namespace}/{$this->route}");
+        if (ltrim($request->get_route(), "/") == "{$this->namespace}/{$this->route}") return true;
+        if (ltrim($request->get_route(), "/") == "{$this->namespace_prefix}{$this->namespace}/{$this->route}") return true;
+
+        return false;
 
     }
 
--- include/objects/view-route.abstract.php
+++ include/objects/view-route.abstract.php
@@ -9,10 +9,6 @@ abstract class View_Route extends Route {
 
     protected $view = View::class;
 
-    protected $namespace_prefix = 'html/';
-
-    protected static $instances = 0;
-
     /* protected function get_params () {
         
         return [
@@ -26,22 +22,6 @@ abstract class View_Route extends Route {
         
     } */
 
-    public function __construct () {
-        
-        parent::__construct();
-
-        if (++static::$instances == 1) add_filter('rest_pre_serve_request', [$this, 'rest_pre_serve_request'], 10, 4);
-        
-    }
-
-    public function register_api_routes () {
-
-        parent::register_api_routes();
-
-        register_rest_route($this->namespace_prefix . $this->namespace, $this->route, $this->rest_args);
-        
-    }
-
     public function callback (WP_REST_Request $request) {
 
         if (!is_subclass_of($this->view, View::class)) return $this->respond(new WP_Error('view-error', "\$view must be a subclass of \Digitalis\View, '{$this->view}' provided."));
@@ -50,33 +30,6 @@ abstract class View_Route extends Route {
         if ($this->rest_args['args']) foreach ($this->rest_args['args'] as $key => $arg) $params[$key] = $request->get_param($key);
 
         return $this->respond(call_user_func("{$this->view}::render", $params, false));
-
-    }
-
-    public function rest_pre_serve_request ($served, $result, $request, $wp_rest_server) {
-
-        if (substr($request->get_route(), 0, strlen($this->namespace_prefix) + 1) == '/' . $this->namespace_prefix) {
-            
-            $embed = isset($_GET['_embed']) ? rest_parse_embed_param($_GET['_embed']) : false;
-            $result = $wp_rest_server->response_to_data($result, $embed);
-    
-            print_r($result);
-
-            return true;
-    
-        }
-    
-        return $served;
-        
-    }
-
-    //
-
-    protected function is_this_route (WP_REST_Request $request) {
-
-        if (parent::is_this_route($request)) return true;
-
-        return (ltrim($request->get_route(), "/") == "{$this->namespace_prefix}{$this->namespace}/{$this->route}");
 
     }
 
