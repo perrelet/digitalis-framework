00b234dd73b864e7b1638dc4fc48b71da2694333
Jamie <jperrelet@gmail.com>
2023-11-02 17:21:13 +0000

    feat: `render_view` method 🏔️➡️🖼️

--- include/objects/view-route.abstract.php
+++ include/objects/view-route.abstract.php
@@ -22,6 +22,12 @@ abstract class View_Route extends Route {
         
     } */
 
+    public function render_view ($params) {
+    
+        return call_user_func("{$this->view}::render", $params, false);
+    
+    }
+
     public function callback (WP_REST_Request $request) {
 
         if (!is_subclass_of($this->view, View::class)) return $this->respond(new WP_Error('view-error', "\$view must be a subclass of \Digitalis\View, '{$this->view}' provided."));
@@ -29,7 +35,7 @@ abstract class View_Route extends Route {
         $params = [];
         if ($this->rest_args['args']) foreach ($this->rest_args['args'] as $key => $arg) $params[$key] = $request->get_param($key);
 
-        return $this->respond(call_user_func("{$this->view}::render", $params, false));
+        return $this->respond($this->render_view($params));
 
     }
 
