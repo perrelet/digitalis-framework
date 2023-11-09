7db2b8d38c263fe2289a2dac7eabedcd29060a34
Jamie <jperrelet@gmail.com>
2023-11-09 23:52:25 +0000

    feat: `App` class 🖥️💾👢

---
+++ include/objects/app.abstract.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Digitalis;
+
+use ReflectionClass;
+
+abstract class App extends Singleton {
+
+    use Autoloader;
+
+    protected $path;
+    protected $url;
+
+    public function __construct () {
+
+        $reflector = new ReflectionClass(static::class);
+        $this->path = plugin_dir_path($reflector->getFileName());
+        $this->url  = plugin_dir_url($this->path);
+
+        add_action('plugins_loaded', [$this, 'load']);
+        if (is_admin()) add_action('plugins_loaded', [$this, 'load_admin']);
+
+    }
+
+    public function load () {
+    
+        $this->autoload($this->path . 'utils', false);
+        $this->autoload($this->path . 'post-types');
+        $this->autoload($this->path . 'taxonomies');
+        $this->autoload($this->path . 'integrations');
+        $this->autoload($this->path . 'models', false);
+        $this->autoload($this->path . 'views', false);
+        $this->autoload($this->path . 'acf-blocks', true);
+        $this->autoload($this->path . 'shortcodes', true);
+        $this->autoload($this->path . 'features', true);
+    
+    }
+
+    public function load_admin () {
+    
+        
+    
+    }
+
+}
