2ebb045cd6ec54434e763b39a89a9e43e12586f2
Jamie <jperrelet@gmail.com>
2023-11-21 20:59:43 +0000

    break: column handling `columns (&$columns)` 🏛️🏛️🏛️

--- include/admin/posts-table.abstract.php
+++ include/admin/posts-table.abstract.php
@@ -8,7 +8,7 @@ abstract class Posts_Table extends Admin_Table {
 
     public function run () {
     
-        add_filter("manage_{$this->post_type}_posts_columns",       [$this, 'columns']);
+        add_filter("manage_{$this->post_type}_posts_columns",       [$this, 'columns_wrap']);
         add_action("manage_{$this->post_type}_posts_custom_column", [$this, 'column'], 10, 2);
 
         //if (!in_array('publish_month', $this->get_filters())) add_filter('disable_months_dropdown', [$this, 'disable_months_dropdown'], 10, 2);
@@ -17,10 +17,57 @@ abstract class Posts_Table extends Admin_Table {
     
     }
 
-    public function columns ($columns) {
+    protected $columns;
+
+    public function columns_wrap ($columns) {
+
+        $this->columns = &$columns;
+        $this->columns($this->columns);
 
         return $columns;
         
+    }
+
+    public function columns (&$columns) {
+
+        // ...
+
+    }
+
+    protected function remove_column ($key) {
+    
+        if (isset($this->columns[$key])) unset($this->columns[$key]);
+    
+    }
+
+    protected function insert_column ($entry, $position = 0, $after = true) {
+
+        if (!is_int($position)) $position = array_search($position, array_keys($this->columns));
+
+        if ($position === false) return;
+        if (!is_array($entry))   return;
+        if ($after) $position++;
+
+        $this->columns =
+            array_slice($this->columns, 0, $position, true) +
+            $entry +
+            array_slice($this->columns, $position, count($this->columns) - 1, true)
+        ;
+    
+    }
+
+    protected function append_column ($entry, $label = null) {
+
+        if (!is_array($entry)) $entry = [$entry => $label];
+        $this->columns += $entry;
+
+    }
+
+    protected function prepend_column ($entry, $label) {
+
+        if (!is_array($entry)) $entry = [$entry => $label];
+        $this->insert_column($entry, 1, false);
+
     }
 
     public function column ($column, $post_id) {
