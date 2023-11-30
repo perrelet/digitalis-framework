508ab9e3f2b14a2e49ca56c839610252f8395771
Jamie <jperrelet@gmail.com>
2023-11-30 00:17:03 +0000

    feat: `Order extends WC_Order` 🏪🛒🛍️

---
+++ include/woocommerce/order.abstract.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Digitalis;
+
+use WC_Order;
+
+class Order extends WC_Order { // Requires use of 'woocommerce_order_class' filter
+
+    public static function get_instance ($id = null) {
+    
+        return wc_get_order($id);
+    
+    }
+
+    public static function inst ($id = null) {
+    
+        return static::get_instance($id);
+    
+    }
+
+}
