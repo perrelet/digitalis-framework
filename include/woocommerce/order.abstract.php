9e8d090749f76f79971c9913e27e96031ea8ed50
Jamie <jperrelet@gmail.com>
2023-12-01 18:24:34 +0000

    refactor: extend from `Model` (rather than `WC_Order`) 🙆‍♀️💃🛒

--- include/woocommerce/order.abstract.php
+++ include/woocommerce/order.abstract.php
@@ -4,17 +4,49 @@ namespace Digitalis;
 
 use WC_Order;
 
-class Order extends WC_Order { // Requires use of 'woocommerce_order_class' filter - Consider: extend from Digitalis\Model instead...
+class Order extends Model {
 
-    public static function get_instance ($id = null) {
+    protected $wc_order;
+
+    public static function extract_id ($data = null) {
+
+        if ($data instanceof WC_Order) return $data->get_id();
+
+        return parent::extract_id($data);
+
+    }
+
+    public static function validate ($data, $uid, $id) {
+
+        if (!($data instanceof WC_Order)) return (bool) wc_get_order($id);
+
+    }
+
+    public function __construct ($data = null, $uid = null, $id = null) {
+
+        parent::__construct($data, $uid, $id);
+
+        if ($this->data instanceof WC_Order) {
+
+            $this->wc_order = $this->data;
+
+        } else {
+
+            $this->wc_order = wc_get_order($this->id);
+
+        }
+
+    }
+
+    public function __call ($name, $args) {
     
-        return wc_get_order($id);
+        return call_user_func_array([$this->wc_order, $name], $args);
     
     }
 
-    public static function inst ($id = null) {
+    public function get_wc_order () {
     
-        return static::get_instance($id);
+        return $this->wc_order;
     
     }
 
