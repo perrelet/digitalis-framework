94a73d94c6f89b65bde8cd270e8a0a3834e88dac
Jamie <jperrelet@gmail.com>
2024-05-24 16:21:54 +0000

    feat: `Number` field 🧮🌾

--- load.php
+++ load.php
@@ -81,6 +81,7 @@ require DIGITALIS_FRAMEWORK_PATH . 'include/views/field-group.view.php';
 
 require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/input.field.php';
 require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/hidden.field.php';
+require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/number.field.php';
 require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/button.field.php';
 require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/submit.field.php';
 require DIGITALIS_FRAMEWORK_PATH . 'include/views/fields/checkbox.field.php';
