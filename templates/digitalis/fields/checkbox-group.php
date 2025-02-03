b97190f3ae8b155f5e73ea829fa0b591d30b55f7
Jamie <jperrelet@gmail.com>
2025-02-03 17:51:44 +0000

    fix: Add ids to options in `Checkbox_Group` template ➕💳⚙️

--- templates/digitalis/fields/checkbox-group.php
+++ templates/digitalis/fields/checkbox-group.php
@@ -1,7 +1,7 @@
 <input type='hidden' name='<?= $key ?>' value='0'>
 <?php $i = 0; if ($options) foreach ($options as $option => $option_label): ?>
-    <label><?= $option_label ?>
-        <input type='checkbox' id='<?= $id ?>' value='<?= $option ?>' name='<?= $key ?>' <?= $i ? $once_atts : '' ?> <?= $attributes ?><?= $option_atts[$option]['html'] ?? '' ?>>
+    <label for='<?= $id ?>-<?= $option ?>'><?= $option_label ?>
+        <input type='checkbox' id='<?= $id ?>-<?= $option ?>' value='<?= $option ?>' name='<?= $key ?>' <?= $i ? $once_atts : '' ?> <?= $attributes ?><?= $option_atts[$option]['html'] ?? '' ?>>
         <span class='checkmark'></span>
     </label>
 <?php $i++;   endforeach; ?>
