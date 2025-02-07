4d72fdce1319d82cb110329a52314c8fa6027e77
Jamie <jperrelet@gmail.com>
2025-02-07 00:19:14 +0000

    feat: `File` field 📑🌾🚜

---
+++ include/views/fields/file.field.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Digitalis\Field;
+
+class File extends \Digitalis\Field {
+
+    protected static $template = 'input';
+
+    protected static $defaults = [
+        'type'     => 'file',
+        'accept'   => [],
+        'multiple' => false,
+    ];
+
+    public static function params ($p) {
+
+        if ($p['accept']) $p['attributes']['accept'] = is_array($p['accept']) ? implode(', ', $p['accept']) : $p['accept'];
+
+        if ($p['multiple']) $p['attributes']['multiple'] = 'multiple';
+    
+        return parent::params($p);
+    
+    }
+
+}
