17566002af07f0900ecc96d8433d25858ec26949
Jamie <jperrelet@gmail.com>
2025-02-21 15:45:27 +0000

    feat: New `Input` params `maxlength` & `minlength` 📏🤏💪

--- include/views/fields/input.field.php
+++ include/views/fields/input.field.php
@@ -6,4 +6,18 @@ use Digitalis\Field;
 
 class Input extends Field {
 
+    protected static $defaults = [
+        'maxlength' => null,
+        'minlength' => null,
+    ];
+
+    public static function params ($p) {
+    
+        if (!is_null($p['maxlength'])) $p['attributes']['maxlength'] = $p['maxlength'];
+        if (!is_null($p['minlength'])) $p['attributes']['minlength'] = $p['minlength'];
+    
+        return parent::params($p);
+    
+    }
+
 }
