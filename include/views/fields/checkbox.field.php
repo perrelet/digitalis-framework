4a56c7b341a3fc06a22e1f8264ec55b6a4f34dd2
Jamie <jperrelet@gmail.com>
2025-02-26 17:28:51 +0000

    fix: `Checkbox` value ✅💎

--- include/views/fields/checkbox.field.php
+++ include/views/fields/checkbox.field.php
@@ -7,15 +7,18 @@ class Checkbox extends Input {
     protected static $template = 'checkbox';
 
     protected static $defaults = [
-        'type'       => 'checkbox',
-        'null_value' => 0,
+        'type'          => 'checkbox',
+        'null_value'    => 0,
+        'checked_value' => 1,
     ];
 
     public static function params ($p) {
     
         $p = parent::params($p);
 
-        if ($p['value']) $p['element']->set_attribute('checked', 'checked');
+        if ($p['value']) $p['element']['checked'] = 'checked';
+
+        $p['element']['value'] = $p['checked_value'];
 
         return $p;
     
