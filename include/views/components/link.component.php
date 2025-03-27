8461f3529eacfc53b12bc38fbf8c5c996e4a001d
Jamie <jperrelet@gmail.com>
2025-03-27 23:53:41 +0000

    fix: Not every `Link` is abstract ❌🔗🎭

--- include/views/components/link.component.php
+++ include/views/components/link.component.php
@@ -2,7 +2,7 @@
 
 namespace Digitalis\Component;
 
-abstract class Link extends \Digitalis\Component {
+class Link extends \Digitalis\Component {
 
     protected static $defaults = [
         'tag'  => 'a',
