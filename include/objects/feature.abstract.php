7624a171c8c18b288b52799c4fb943d5bb297303
Jamie <jperrelet@gmail.com>
2025-04-08 19:49:59 +0000

    feat: Implement `Has_WP_Hooks` into `Feature` and `Integration` 🎣✨🤝

--- include/objects/feature.abstract.php
+++ include/objects/feature.abstract.php
@@ -4,20 +4,29 @@ namespace Digitalis;
 
 abstract class Feature extends Factory {
 
+    use Has_WP_Hooks;
+
     public static function load () {
-    
+
         return call_user_func_array([static::class, 'get_instance'], func_get_args());
-    
+
     }
 
     protected static function construct_instance ($instance, $data = []) {
-    
+
         parent::construct_instance($instance, $data);
 
+        $instance->add_hooks((array) $instance->get_hooks());
         $instance->run();
-    
+
     }
 
     public function run () {}
+
+    public function get_hooks () {
+
+        return [];
+
+    }
 
 }
--- include/objects/integration.abstract.php
+++ include/objects/integration.abstract.php
@@ -4,14 +4,23 @@ namespace Digitalis;
 
 abstract class Integration extends Singleton {
 
+    use Has_WP_Hooks;
+
     protected static function construct_instance ($instance) {
-    
+
         parent::construct_instance($instance);
 
+        $instance->add_hooks((array) $instance->get_hooks());
         $instance->run();
-    
+
     }
     
     public function run () {}
+
+    public function get_hooks () {
+
+        return [];
+
+    }
 
 }
