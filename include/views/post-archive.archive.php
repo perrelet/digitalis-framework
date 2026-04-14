5fcbba3231c1d14e04e2260fdb13a12f20facafe
Jamie <jperrelet@gmail.com>
2026-04-14 21:17:49 +0000

    fix: `Post_Archive::get_page_links` elegant fallback for pagination urls 🦢🍂📄

--- include/views/post-archive.archive.php
+++ include/views/post-archive.archive.php
@@ -11,16 +11,30 @@ class Post_Archive extends Archive {
         'item_model' => Post::class,
     ];
 
-    public function get_page_links ($query) {
+    public function get_page_links ($wp_query) {
 
-        if (!($query instanceof WP_Query) || !($query->max_num_pages > 1)) return [];
-    
-        return paginate_links(wp_parse_args($this['paginate_args'], [
-            'current'   => max(1, $query->get('paged')),
-            'total'     => $query->max_num_pages,
+        if (!($wp_query instanceof WP_Query) || !($wp_query->max_num_pages > 1)) return [];
+
+        $args = wp_parse_args($this->paginate_args, [
+            'current'   => max(1, $wp_query->get('paged')),
+            'total'     => $wp_query->max_num_pages,
             'type'      => 'array',
-        ]));
-    
+        ]);
+
+        if ((wp_doing_ajax() || defined('REST_REQUEST')) && ($referer = wp_get_referer())) {
+
+            $parts          = parse_url($referer);
+            $path           = preg_replace('#/page/\d+/?#', '/', $parts['path'] ?? '/');
+            $params         = array_diff_key($_POST, array_flip(['action', 'nonce', 'paged']));
+            $query          = http_build_query($params);
+            $args['base']   = trailingslashit($path) . '%_%';
+            $args['format'] = 'page/%#%/' . ($query ? "?{$query}" : '');
+            $args['add_args'] = [];
+
+        }
+
+        return paginate_links($args);
+
     }
 
 }
